a8629854620a2f4d5f12848156fb98b6b0db09c5
Erik Nordstr?m <erik.nordstrom@gmail.com>
2010-03-24 12:53:12 -0400

    * Added support for openjdk when detecting path of jni.h

---
+++ src/hagglekernel/Bloomfilter.h
@@ -0,0 +1,150 @@
+/* Copyright 2008-2009 Uppsala University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _HAGGLE_BLOOMFILTER_H
+#define _HAGGLE_BLOOMFILTER_H
+
+/*
+	Forward declarations of all data types declared in this file. This is to
+	avoid circular dependencies. If/when a data type is added to this file,
+	remember to add it here.
+*/
+
+class Bloomfilter;
+
+#include "Debug.h"
+#include "DataObject.h"
+
+using namespace haggle;
+
+/** */
+#ifdef DEBUG_LEAKS
+class Bloomfilter : public LeakMonitor
+#else
+class Bloomfilter
+#endif
+{
+public:
+	typedef enum {
+		BF_TYPE_NORMAL,
+		BF_TYPE_COUNTING
+	} BloomfilterType_t;
+private:
+	BloomfilterType_t type;
+	float error_rate;
+	unsigned int capacity;
+	const unsigned long init_n;
+	union {
+		struct bloomfilter *bf;
+		struct counting_bloomfilter *cbf;
+		unsigned char *raw;
+	};
+	Bloomfilter(float _error_rate, unsigned int _capacity, struct bloomfilter *bf);
+	Bloomfilter(float _error_rate, unsigned int _capacity, struct counting_bloomfilter *cbf);
+public:
+	/**
+		Creates a bloomfilter with the given error rate and capacity.
+	*/
+	Bloomfilter(float error_rate, unsigned int capacity, bool counting = false);
+
+	
+	Bloomfilter(const unsigned char *bf, size_t len);
+
+	/**
+		Creates an identical copy of the given bloomfilter.
+	*/
+	Bloomfilter(const Bloomfilter &bf);
+	/**
+		Destroys the bloomfilter.
+	*/
+	~Bloomfilter();
+	
+	BloomfilterType_t getType() const { return type; }
+	/**
+		Adds the given data object to the bloomfilter.
+	*/
+	void add(const DataObjectRef &dObj);
+	/**
+		Removes the given data object from the bloomfilter. Only works on 
+		counting bloomfilters. For non-counting bloomfilters, this function 
+		does nothing.
+	*/
+	void remove(const DataObjectRef &dObj);
+	/**
+		Returns true iff the data object is in the bloomfilter.
+	*/
+	
+	bool merge(const Bloomfilter& bf_merge);
+	
+	bool has(const DataObjectRef &dObj) const;
+	/**
+		Returns a newly allocated non counting bloomfilter based
+		on a counting bloomfilter.
+		
+		Returns: A non-counting version of the bloomfilter if
+		the original bloomfilter was a counting one. If the 
+		original bloomfilter was no a counting one, the function 
+		simply returns copy of the bloomfilter.
+		If an error occurs, the function returns NULL.
+	*/
+	Bloomfilter *to_noncounting() const;
+	/**
+		Returns a platform-independent representation of the bloomfilter in a
+		Base64 encoded string.
+	*/
+	string toBase64() const;
+	/**
+		Sets the bloomfilter to be the bloomfilter represented by the given
+		Base64 encoded string.
+		
+		Can only set a non-counting bloomfilter to a non-counting bloomfilter
+		and a counting bloomfilter to a counting bloomfilter.
+	*/
+	void fromBase64(const string &b64);
+	
+	/**
+		Returns a platform-independent representation of the bloomfilter in a
+		Base64 encoded string.
+		
+		For non-counting bloomfilters, this is exactly the same as toBase64().
+		
+		For counting bloomfilters, this function returns what the toBase64() 
+		function would return for a non-counting bloomfilter with the same 
+		inserted data objects as this bloomfilter. This means
+	*/
+	string toBase64NonCounting() const;
+	
+	/**
+		Returns the number of data objects in the bloomfilter.
+	*/
+	unsigned long numObjects() const;
+	
+	/**
+	*/
+	const unsigned char *getRaw() const;
+	/**
+	*/
+	size_t getRawLen(void) const;
+	
+	/**
+	*/
+	bool setRaw(const unsigned char *bf, size_t bf_len);
+	
+	/**
+		Clears the bloomfilter.
+	*/
+	void reset();
+};
+
+#endif /* _HAGGLE_BLOOMFILTER_H */
---
+++ src/hagglekernel/ConnectivityBluetooth.cpp
@@ -0,0 +1,216 @@
+/* Copyright 2008-2009 Uppsala University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <libcpphaggle/Platform.h>
+
+#if defined(ENABLE_BLUETOOTH)
+
+#include "ConnectivityBluetooth.h"
+
+ConnectivityBluetooth::ConnectivityBluetooth(ConnectivityManager *m, const InterfaceRef& iface) :
+	ConnectivityBluetoothBase(m, iface, "Bluetooth connectivity")
+{
+	LOG_ADD("%s: Bluetooth connectivity starting. Scan time: %ld +- %ld seconds\n",
+		Timeval::now().getAsString().c_str(), baseTimeBetweenScans, randomTimeBetweenScans);
+}
+
+ConnectivityBluetooth::~ConnectivityBluetooth()
+{
+	LOG_ADD("%s: Bluetooth connectivity stopped.\n",
+		Timeval::now().getAsString().c_str());
+}
+
+ConnectivityBluetoothBase::ConnectivityBluetoothBase(ConnectivityManager *m, const InterfaceRef& iface, const string name) :
+	Connectivity(m, iface, name)
+{
+}
+
+ConnectivityBluetoothBase::~ConnectivityBluetoothBase()
+{
+
+}
+
+Mutex ConnectivityBluetoothBase::sdpListMutex;
+InterfaceRefList ConnectivityBluetoothBase::sdpWhiteList;
+InterfaceRefList ConnectivityBluetoothBase::sdpBlackList;
+bool ConnectivityBluetoothBase::ignoreNonListedInterfaces = false;
+unsigned long ConnectivityBluetoothBase::baseTimeBetweenScans = DEFAULT_BASE_TIME_BETWEEN_SCANS;
+unsigned long ConnectivityBluetoothBase::randomTimeBetweenScans = DEFAULT_RANDOM_TIME_BETWEEN_SCANS;
+
+int ConnectivityBluetoothBase::classifyAddress(const Interface &iface)
+{
+	Mutex::AutoLocker l(sdpListMutex);
+	
+	for (InterfaceRefList::iterator it = sdpWhiteList.begin();
+		it != sdpWhiteList.end(); it++) {
+		if (*it == iface)
+			return BLUETOOTH_ADDRESS_IS_HAGGLE_NODE;
+	}
+	if (ignoreNonListedInterfaces)
+		return BLUETOOTH_ADDRESS_IS_NOT_HAGGLE_NODE;
+	
+	for (InterfaceRefList::iterator it = sdpBlackList.begin();
+		it != sdpBlackList.end(); it++) {
+		if (*it == iface)
+			return BLUETOOTH_ADDRESS_IS_NOT_HAGGLE_NODE;
+	}
+	return BLUETOOTH_ADDRESS_IS_UNKNOWN;
+}
+
+int ConnectivityBluetoothBase::classifyAddress(const InterfaceType_t type, const unsigned char *identifier)
+{
+	Interface iface(type, identifier);
+	
+	return classifyAddress(iface);
+}
+
+void ConnectivityBluetoothBase::clearSDPLists(void)
+{
+	Mutex::AutoLocker l(sdpListMutex);
+	sdpBlackList.clear();
+	sdpWhiteList.clear();
+}
+
+void ConnectivityBluetoothBase::updateSDPLists(Metadata *md)
+{
+	Mutex::AutoLocker l(sdpListMutex);
+
+	const char *param = md->getParameter("scan_base_time");
+
+	if (param) {
+		char *endptr = NULL;
+		unsigned long base_time = strtoul(param, &endptr, 10);
+
+		if (endptr && endptr != param) {
+			baseTimeBetweenScans = base_time;
+			LOG_ADD("# ConnectivityManager: setting Bluetooth scan base time %lu\n", baseTimeBetweenScans);
+		}
+	}
+
+	param = md->getParameter("scan_random_time");
+
+	if (param) {
+		char *endptr = NULL;
+		unsigned long random_time = strtoul(param, &endptr, 10);
+
+		if (endptr && endptr != param) {
+			randomTimeBetweenScans = random_time;
+			LOG_ADD("# ConnectivityManager: setting Bluetooth scan random time %lu\n", randomTimeBetweenScans);
+		}
+	}
+	/*
+	 Check bluetooth module blacklisting/whitelisting data. Formatted like so:
+	 
+	 <Bluetooth>
+	 <ClearBlacklist/>
+	 <ClearWhitelist/>
+	 <Blacklist>
+	 <Interface>bt://11:22:33:44:55:66</interface>
+	 </Blacklist>
+	 <Whitelist>
+	 <Interface>bt://77:88:99:AA:BB:CC</interface>
+	 </Whitelist>
+	 <IgnoreNonListedInterfaces>yes</IgnoreNonListedInterfaces>
+	 </Bluetooth>
+	 
+	 Of course, all components of the bluetooth section are optional, and it 
+	 is possible to insert any number of blacklisted/whitelisted interfaces.
+	 
+	 It is important for the interface addresses to have the bt:// prefix. If 
+	 they do not, then the interfaces will not be accepted.
+	 */
+	
+	Metadata *bl = md->getMetadata("Blacklist");
+	Metadata *wl = md->getMetadata("Whitelist");
+	Metadata *ignore = md->getMetadata("IgnoreNonListedInterfaces");
+	
+	Metadata *cbl = md->getMetadata("ClearBlacklist");
+	Metadata *cwl = md->getMetadata("ClearWhitelist");
+	
+	if (cbl)
+		sdpBlackList.clear();
+	
+	if (cwl)
+		sdpWhiteList.clear();
+	
+	if (bl)  {
+		InterfaceRef i;		
+		Metadata *iface = bl->getMetadata("Interface");
+		
+		while (iface) {
+			const char *type = iface->getParameter("type");
+			const char *name = iface->getParameter("name") ? iface->getParameter("name") : "noname";
+
+			if (!type) {
+				HAGGLE_ERR("No type specified for interface \'%s\'... ignoring\n", name);
+			} else if (strcmp(type, "bluetooth") != 0) {
+				HAGGLE_ERR("Interface type is \'%s\', and not \'bluetooth\'\n", type);
+			} else {			
+				Address a(iface->getContent().c_str());
+				if (a.getType() == AddressType_BTMAC) {
+					i = new Interface(IFTYPE_BLUETOOTH, a.getRaw(), &a);
+
+					if (i) {
+						sdpBlackList.push_back(i);
+						LOG_ADD("# ConnectivityManager: black-listing interface [type=%s identifier=%s name=%s]\n", 
+							type, i->getIdentifierStr(), name);
+					}
+				}
+			}
+			iface = bl->getNextMetadata();
+		}
+	}
+
+	if (wl) {
+		InterfaceRef i;
+		Metadata *iface = wl->getMetadata("Interface");
+		
+		while (iface) {
+			const char *type = iface->getParameter("type");
+			const char *name = iface->getParameter("name") ? iface->getParameter("name") : "noname";
+
+			if (!type) {
+				HAGGLE_ERR("No type specified for interface \'%s\'... ignoring\n", name);
+			} else if (strcmp(type, "bluetooth") != 0) {
+				HAGGLE_ERR("Interface type is \'%s\', and not \'bluetooth\'\n", type);
+			} else {
+				Address a(iface->getContent().c_str());
+				if (a.getType() == AddressType_BTMAC) {
+					i = new Interface(IFTYPE_BLUETOOTH, a.getRaw(), &a);
+
+					if (i) {
+						sdpWhiteList.push_back(i);
+
+						LOG_ADD("# ConnectivityManager: white-listing interface [type=%s address=%s name=%s]\n", 
+							type, i->getIdentifierStr(), name);
+					}
+				}
+			}
+			iface = wl->getNextMetadata();
+		}
+	}
+	
+	if (ignore) {
+		if (ignore->getContent() == "yes")
+			ignoreNonListedInterfaces = true;
+		else if(ignore->getContent() == "no")
+			ignoreNonListedInterfaces = false;
+		else {
+			HAGGLE_ERR("IgnoreNonListedInterfaces content wrong. Must be yes/no.\n");
+		}
+	}
+}
+
+#endif
