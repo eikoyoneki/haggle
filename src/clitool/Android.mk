084a3551fc429f36d1c32c4256329ed184246e92
Erik Nordstr?m <erik.nordstrom@gmail.com>
2010-08-04 15:50:13 -0400

    * Fix compilation problems when using --enable-bundled-sqlite in Linux

---
+++ src/clitool/Android.mk
@@ -0,0 +1,33 @@
+# Copyright 2008 Uppsala University
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Do not build for simulator
+ifneq ($(TARGET_SIMULATOR),true)
+
+#
+# clitool
+#
+LOCAL_SRC_FILES := \
+	clitool.cpp \
+	../utils/thread.c
+
+LOCAL_C_INCLUDES += \
+        $(LOCAL_PATH)/../utils \
+        $(LOCAL_PATH)/../libhaggle/include
+
+LOCAL_LDLIBS := -lpthread -lhaggle
+LOCAL_SHARED_LIBRARIES := libdl libstdc++ libhaggle
+
+LOCAL_STATIC_LIBRARIES +=
+
+LOCAL_CFLAGS:=-O2 -g
+LOCAL_CPPFLAGS+=-DOS_ANDROID -DOS_LINUX -DDEBUG
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE := clitool
+
+include $(BUILD_EXECUTABLE)
+
+endif
---
+++ src/luckyMe/Android.mk
@@ -0,0 +1,32 @@
+# Copyright 2008 Uppsala University
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Do not build for simulator
+ifneq ($(TARGET_SIMULATOR),true)
+
+#
+# LuckyMe
+#
+LOCAL_SRC_FILES := \
+	luckyme.c 
+
+LOCAL_C_INCLUDES += \
+        $(LOCAL_PATH)/../utils \
+        $(LOCAL_PATH)/../libhaggle/include
+
+LOCAL_LDLIBS := -lpthread -lhaggle
+
+LOCAL_SHARED_LIBRARIES = libdl libhaggle
+LOCAL_STATIC_LIBRARIES =
+
+LOCAL_CFLAGS:=-O2 -g
+LOCAL_CPPFLAGS+=-DOS_ANDROID -DDEBUG
+
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE := luckyme
+
+include $(BUILD_EXECUTABLE)
+
+endif
