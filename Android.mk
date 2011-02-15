b7c9904bb30791daf85efedd2cc5977cb70b369d
Erik Nordstr?m <erik.nordstrom@gmail.com>
2011-02-14 22:59:09 -0500

    * Support for running Haggle as an Android service

---
+++ Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# Do not build for simulator
+ifneq ($(TARGET_SIMULATOR),true)
+
+subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
+                extlibs/libxml2-2.6.31 \
+                src/libcpphaggle \
+                src/hagglekernel \
+                src/libhaggle \
+		src/luckyMe \
+		src/clitool \
+		android \
+        ))
+
+include $(subdirs)
+
+endif
---
+++ android/LuckyMe/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# Build all java files in the src subdirectory
+LOCAL_SRC_FILES := $(call all-java-files-under,src)
+
+LOCAL_JAVA_LIBRARIES := org.haggle
+#LOCAL_STATIC_JAVA_LIBRARIES := org.haggle
+
+# Enabling the following line will bundle all necessary libraries with the 
+# application package
+#LOCAL_JNI_SHARED_LIBRARIES += libhaggle_jni libhaggle libhaggle-xml2
+
+# Name of the APK to build
+LOCAL_PACKAGE_NAME := LuckyMe
+
+# Tell it to build an APK
+include $(BUILD_PACKAGE)
+
