0490cb37c3fcb325f8267b7bd223ba663ea327a4
Erik Nordstr?m <erik.nordstrom@gmail.com>
2012-12-15 01:52:24 -0500

    * Compile fixes
    Android:
    
    * Haggle: fix permissions on pid file and logs
    * PhotoShare: fix launch progress dialog

---
+++ src/libhaggle/jni/native/common.h
@@ -0,0 +1,20 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
+#ifndef LIBHAGGLE_JNI_COMMON_H
+#define LIBHAGGLE_JNI_COMMON_H
+
+#include <libhaggle/haggle.h>
+#include <libhaggle/attribute.h>
+#include <libhaggle/node.h>
+#include <jni.h>
+
+#if defined(OS_ANDROID)
+#define JNI_ENV(env) (env)
+#else
+#define JNI_ENV(env) (void **)(env)
+#endif
+
+jobjectArray libhaggle_jni_nodelist_to_node_jobjectArray(JNIEnv *env, haggle_nodelist_t *nl);
+jobjectArray libhaggle_jni_attributelist_to_attribute_jobjectArray(JNIEnv *env, haggle_attrlist_t *al);
+jobjectArray libhaggle_jni_dataobject_to_attribute_jobjectArray(JNIEnv *env, haggle_dobj_t *dobj);
+
+#endif /* LIBHAGGLE_JNI_COMMON_H */
